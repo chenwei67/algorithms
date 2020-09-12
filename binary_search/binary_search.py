f5e8ae86e6cf5dcf0982ab4bcbecc1d40a7917cd
chen wei <chenwei67@foxmail.com>
2020-09-13 01:59:52 +0800

    boundary_search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -15,7 +15,7 @@ mid向下取整时:
 """
 
 
-def binary_search(nums: List[int], target: int):
+def binary_search_v1(nums: List[int], target: int):
     """二分搜索"""
     left, right = 0, len(nums) - 1
     while left <= right:  # 当left=mid=right的结果可能不满足时需要
@@ -29,5 +29,28 @@ def binary_search(nums: List[int], target: int):
     return None  # 未找到
 
 
-print(binary_search([1, 2, 3, 5], 4))
+def binary_search_wrong1(nums: List[int], target: int):
+    left, right = 0, len(nums) - 1
 
+    """当 nums[mid]等于target，死循环"""
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+
+
+def binary_search_v2(nums: List[int], target: int):
+    if not nums:
+        return None
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] >= target:
+            right = mid  # right指向大于等于target的值，并且是"归档"的
+    # 结束时,left==mid==right
+    return nums[left] == target
---
+++ binary_search/boundary_search.py
@@ -0,0 +1,74 @@
+from typing import List
+
+"""
+总结：
+    0.while循环不需要等号
+    1.nums[mid]等于target时，根据是否需要排除mid，分配到"1"或另一个判断句
+    2.mid取整方式取决于"1"在小于还是大于的判断句
+    3.值不排除的时候，不能有"1"，值排除的时候，可以无"1"
+关键：
+    "1"与"nums[mid]==target"，决定了二分法怎么写
+"""
+
+
+def left_boundary_binary_search(nums: List[int], target: int) -> bool:
+    if not nums:
+        return False
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] <= target:  # 等于时，mid是可以排除的
+            left = mid + 1
+        else:
+            right = mid
+    # 结束时,left==mid==right
+    return nums[left] > target
+
+
+def right_boundary_binary_search_wrong1(nums: List[int], target: int) -> bool:
+    if not nums:
+        return False
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        """这时候不能向下取整，否则当前长度为2，即left等于mid时，下面这个if可能死循环"""
+        if nums[mid] <= target:
+            left = mid
+        else:
+            right = mid - 1
+
+
+def right_boundary_binary_restrict(nums: List[int], target: int) -> bool:
+    """
+    这种方式有局限性，只能用于无重复元素。[0, 1, 1, 1]就会失效
+    """
+    if not nums:
+        return False
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        """mid向上取整"""
+        mid = (left + right) // 2 + 1
+        if nums[mid] <= target:
+            left = mid
+        else:
+            right = mid - 1
+    return nums[left] < target
+
+
+def right_boundary_binary_search(nums: List[int], target: int) -> bool:
+    if not nums:
+        return False
+
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2 + 1
+        if nums[mid] < target:
+            left = mid
+        else:  # 等于时，排除mid
+            right = mid - 1
+    return nums[left] < target
+
+print(right_boundary_binary_search([0, 1, 1, 1], 1))
